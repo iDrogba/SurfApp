3b5f6e1bf61166b2131aa06c21aa3e2c4f7ecb2b
김상현 <kshy1019@naver.com>
2023-03-03 23:06:33 +0900

    MainScene 그룹 정리

---
+++ SurfApp/SurfApp/Sources/MainScene/View/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SurfApp
+//
+//  Created by 김상현 on 2023/03/03.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ SurfApp/SurfApp/Sources/MainScene/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  SurfApp
+//
+//  Created by 김상현 on 2023/03/03.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+}
